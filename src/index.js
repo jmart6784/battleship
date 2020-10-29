b9319456ed977c79351b8a08dc8f419fc559fa71
Juan <jmartinez6784@yahoo.com>
2020-10-29 14:54:30 -0700

    Added player 1 ship pieces to UI, removed '.' character from UI buttons, and added a new class to player 1 and player 2 button tiles

--- src/index.js
+++ src/index.js
@@ -1,19 +1,82 @@
+let p1Tiles = document.querySelectorAll(".p1-board-tile");
+let p2Tiles = document.querySelectorAll(".p2-board-tile");
+let moves = [];
+
 export const ship = (length, sunk, coords) => {
   const hit = (x, y) => {
     // Mark x,y coordinate as hit only if ship is hit
   };
 
-  return { length, sunk };
+  return { length, sunk, coords, hit };
 };
 
-let ship1 = ship(4, false, []);
-console.log(ship1.length, ship1.sunk);
+let p1Ship1 = ship(4, false, ["1A", "1B", "1C", "1D"]);
+let p1Ship2 = ship(3, false, ["3A", "3B", "3C"]);
+let p1Ship3 = ship(3, false, ["5A", "5B", "5C"]);
+let p1Ship4 = ship(2, false, ["7A", "7B"]);
+let p1Ship5 = ship(2, false, ["9A", "9B"]);
+let p1Ship6 = ship(2, false, ["2I", "2J"]);
+let p1Ship7 = ship(1, false, ["4J"]);
+let p1Ship8 = ship(1, false, ["6J"]);
+let p1Ship9 = ship(1, false, ["8J"]);
+let p1Ship10 = ship(1, false, ["10J"]);
 
-const board = (() => {
-  let moves = [];
-  let allTiles = document.querySelectorAll(".board-tile");
+let p2Ship1 = ship(4, false, []);
+let p2Ship2 = ship(3, false, []);
+let p2Ship3 = ship(3, false, []);
+let p2Ship4 = ship(2, false, []);
+let p2Ship5 = ship(2, false, []);
+let p2Ship6 = ship(2, false, []);
+let p2Ship7 = ship(1, false, []);
+let p2Ship8 = ship(1, false, []);
+let p2Ship9 = ship(1, false, []);
+let p2Ship10 = ship(1, false, []);
 
-  allTiles.forEach((tile) => {
+const player = (name, isTurn, moves, ships, tiles) => {
+  return { name, isTurn, moves, ships, tiles };
+
+  // Make A.I player that takes turns at random coords but doesn't repeat hit coordinates
+};
+
+let player1 = player(
+  "player1",
+  true,
+  [],
+  [
+    p1Ship1,
+    p1Ship2,
+    p1Ship3,
+    p1Ship4,
+    p1Ship5,
+    p1Ship6,
+    p1Ship7,
+    p1Ship8,
+    p1Ship9,
+    p1Ship10,
+  ],
+  p1Tiles
+);
+let player2 = player(
+  "player2",
+  false,
+  [],
+  [
+    p2Ship1,
+    p2Ship2,
+    p2Ship3,
+    p2Ship4,
+    p2Ship5,
+    p2Ship6,
+    p2Ship7,
+    p2Ship8,
+    p2Ship9,
+    p2Ship10,
+  ],
+  p2Tiles
+);
+
+export const board = (() => {
+  player1.tiles.forEach((tile) => {
     tile.onclick = () => {
       // HTML name tag is split with player and coordinate info
       let nameSpT = tile.name.split(" ");
@@ -22,7 +85,6 @@ const board = (() => {
       let y = nameSpT[1].split("")[1];
 
       moves.push({ player, x, y });
-      console.log("MOOOOVES", moves);
 
       // Disable button from being pressed again
       tile.disabled = true;
@@ -32,6 +94,20 @@ const board = (() => {
 
   const placePieces = () => {
     // Place ship pieces using ship.coords array
+    console.log(p1Ship1.coords);
+
+    player1.ships.forEach((ship) => {
+      ship.coords.forEach((coord) => {
+        player1.tiles.forEach((tile) => {
+          if (tile.name.split(" ")[1] === coord) {
+            tile.style.backgroundColor = "blue";
+          } else {
+            console.log("NO MATCH");
+          }
+          // console.log("SSS", tile);
+        });
+      });
+    });
   };
 
   const receiveAttack = (x, y) => {
@@ -49,15 +125,9 @@ const board = (() => {
   return { placePieces, receiveAttack, getCoords };
 })();
 
-const player = (name, isTurn) => {
-  return { name, isTurn };
-
-  // Make A.I player that takes turns at random coords but doesnt repeat hit coordinates
-};
-
-let player1 = player("player1", true);
-let player2 = player("player2", false);
-
 const gameLoop = () => {
   // Loop game here only using existing methods
+  board.placePieces();
 };
+
+gameLoop();
--- src/index.test.js
+++ src/index.test.js
@@ -1,6 +1,11 @@
-import { ship } from "./index";
+import { ship, board } from "./index";
 
 test("ships are not sunk by default", () => {
   const ship1 = ship(4, false, []);
   expect(ship1.sunk).toBe(false);
+});
+
+test("ship1 length is 4", () => {
+  const ship1 = ship(4, false, []);
+  expect(ship1.length).toBe(4);
 });
